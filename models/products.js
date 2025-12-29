1c68b692479b8d66ee0742cdbdf5fe941fd26657
Ramon Melo <ramonmelo.com@gmail.com>
2025-12-29 12:58:17 -0300

    feat: add the products.js file to the project

---
+++ models/products.js
@@ -0,0 +1,56 @@
+export const PRODUCTS = {
+  "risco-safari": {
+    id: "0",
+    title: "Risco para bordar Safari",
+    price: 1300,
+    description:
+      "Risco digital para bordado Safari, pronto para impressão, com versões personalizáveis e sugestão de pontos.",
+    detailsPage: "/riscos-para-bordar/riscosDetails.html",
+  },
+
+  "risco-jardim-borboleta": {
+    id: "1",
+    title: "Risco para bordar Jardim de Borboleta",
+    price: 1400,
+    description:
+      "Risco digital Jardim de Borboleta em PDF, ideal para iniciantes, com versão com e sem frase e sugestão de pontos.",
+    detailsPage: "/riscos-para-bordar/riscosDetails.html",
+  },
+
+  "risco-amor-transbordou": {
+    id: "2",
+    title: "Risco para bordar O amor Transbordou",
+    price: 1000,
+    description:
+      "Risco digital O Amor Transbordou em PDF, com frase e sugestão de pontos, uso liberado para bordados vendidos.",
+    detailsPage: "/riscos-para-bordar/riscosDetails.html",
+  },
+
+  "risco-harry-potter": {
+    id: "3",
+    title: "Risco para bordar Harry Potter",
+    price: 1200,
+    description:
+      "Risco digital inspirado em Harry Potter, pronto para impressão, uso liberado para venda do bordado final.",
+    detailsPage: "/riscos-para-bordar/riscosDetails.html",
+  },
+
+  "risco-lar-doce-lar": {
+    id: "4",
+    title: "Risco para bordar Lar Doce Lar",
+    price: 1300,
+    description:
+      "Risco digital Lar Doce Lar com versões personalizáveis e sugestão de pontos, pronto para impressão.",
+    detailsPage: "/riscos-para-bordar/riscosDetails.html",
+  },
+
+  "curso-bordado-pingente-natal": {
+    id: "5",
+    title:
+      "Curso de Iniciação ao Bordado Livre – Crie Seu Próprio Pingente de Natal",
+    price: 2600,
+    description:
+      "Curso introdutório de bordado livre com projeto prático de Natal, ensinando fundamentos essenciais e finalização em dois modelos de bastidor.",
+    detailsPage: "/cursos/index.html",
+  },
+};
